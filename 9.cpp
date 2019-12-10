f1057d36a7a298e88e8f660e44e7ad47cd2d6b40
David Tracy <david.tracy3@gmail.com>
2019-12-09 22:02:44 -0700

    made puzzle 9 fancier

--- 9.cpp
+++ 9.cpp
@@ -8,12 +8,11 @@ int main(int argc, char *argv[]) {
   assert(argc == 2);
 
   Intcode ic(argv[1]);
-  ic.push_input({1});
-  ic.run_program();
-  cout << "Part 1: " << ic.get_output() << endl;
-  ic.push_input({2});
-  ic.run_program();
-  cout << "Part 2: " << ic.get_output() << endl;
+  for (auto &x : {1, 2}) {
+    ic.push_input({x});
+    ic.run_program();
+    cout << "Part " << x << ": " << ic.get_output() << endl;
+  }
 
   return 0;
 }
