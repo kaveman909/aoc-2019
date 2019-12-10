b9da09665a32db8726f3fbb9a4d597297de405b1
David Tracy <david.tracy3@gmail.com>
2019-12-10 09:57:30 -0700

    finished day 10

--- 10.cpp
+++ 10.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <numbers>
 #include <string>
 #include <vector>
 
@@ -11,7 +12,42 @@ using namespace std;
 
 vector<vector<int>> map(0);
 
-int visible(size_t ia, size_t ja) {
+struct AstInfo {
+  double angle;
+  int visible;
+};
+
+int destroy_ast(size_t ia, size_t ja, double angle) {
+  int x200 = 0;
+  int y200 = 0;
+  double mag = 100000;
+  for (size_t i = 0; i < map.size(); i++) {
+    for (size_t j = 0; j < map.front().size(); j++) {
+      // don't consider yourself as visible, and we don't care about
+      // non-asteroid spots
+      if (((ia == i) && (ja == j)) || !map[i][j]) {
+        continue;
+      }
+      int y = i - ia;
+      int x = j - ja;
+      double mag2 = sqrt(x * x + y * y);
+      double angle2 = atan2(x, -y);
+      if (angle2 < 0) {
+        angle2 += 2 * numbers::pi;
+      }
+      if (angle2 == angle) {
+        if (mag2 < mag) {
+          mag = mag2;
+          x200 = j;
+          y200 = i;
+        }
+      }
+    }
+  }
+  return x200 * 100 + y200;
+}
+
+AstInfo visible(size_t ia, size_t ja) {
   vector<double> angles(map.size() * map.front().size());
 
   for (size_t i = 0; i < map.size(); i++) {
@@ -21,15 +57,26 @@ int visible(size_t ia, size_t ja) {
       if (((ia == i) && (ja == j)) || !map[i][j]) {
         continue;
       }
-      int deltai = ia - i;
-      int deltaj = ja - j;
-      angles[i * map.size() + j] = atan2(deltaj, deltai);
+      int y = i - ia;
+      int x = j - ja;
+      double angle = atan2(x, -y);
+      if (angle < 0) {
+        angle += 2 * numbers::pi;
+      }
+      angles[i * map.size() + j] = angle;
     }
   }
   // sort the angles for the upcoming unique()
   sort(angles.begin(), angles.end());
 
-  return distance(angles.begin(), unique(angles.begin(), angles.end()));
+  auto unique_it = unique(angles.begin(), angles.end());
+  double angle_200 = angles.size() >= 200 ? angles[199] : 0;
+
+  AstInfo ai;
+  ai.angle = angle_200;
+  ai.visible = distance(angles.begin(), unique_it);
+
+  return ai;
 }
 
 int main(int argc, char *argv[]) {
@@ -45,6 +92,9 @@ int main(int argc, char *argv[]) {
   }
 
   int max_asteroids = 0;
+  double angle_200 = 0;
+  int i_max = 0;
+  int j_max = 0;
   for (size_t i = 0; i < map.size(); i++) {
     for (size_t j = 0; j < map.front().size(); j++) {
       // if it's not an asteroid, continue
@@ -52,12 +102,16 @@ int main(int argc, char *argv[]) {
         continue;
       }
       // found an asteroid; find how many it can see
-      int vis = visible(i, j);
-      if (vis > max_asteroids) {
-        max_asteroids = vis;
+      AstInfo ai = visible(i, j);
+      if (ai.visible > max_asteroids) {
+        max_asteroids = ai.visible;
+        i_max = i;
+        j_max = j;
+        angle_200 = ai.angle;
       }
     }
   }
   cout << "Part 1: " << max_asteroids << endl;
+  cout << "Part 2: " << destroy_ast(i_max, j_max, angle_200) << endl;
   return 0;
 }
